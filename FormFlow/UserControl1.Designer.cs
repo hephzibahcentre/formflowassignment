c35555713e5cc417502e6cfd8203c721fff98f10
hephzibahcentre <114697769+hephzibahcentre@users.noreply.github.com>
2022-10-07 10:04:17 +0100

    Folder commit

---
+++ FormFlow/UserControl1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace FormFlow
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a number:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(117, 33);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(117, 59);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(46, 137);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(65, 23);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "btn1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(117, 137);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(65, 23);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "btn2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(188, 137);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(65, 23);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "btn3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(235, 64);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(49, 13);
+            this.lblOutput.TabIndex = 6;
+            this.lblOutput.Text = "lblOutput";
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(319, 235);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
---
+++ FormFlow/UserControl1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormFlow
+{
+    public partial class UserControl1: UserControl
+    {
+        decimal decNumber;
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decNumber = Convert.ToDecimal(txtNumber.Text);
+            lblOutput.Text = decNumber.ToString();
+            if (decNumber >= 15)
+            {
+                this.BackColor = Color.Red;
+                btn1.Visible = true;
+                btn2.Visible = true;
+                btn3.Visible = true;
+            } else
+            {
+                this.BackColor = Color.Blue;
+                btn1.Visible = false;
+                btn2.Visible = false;
+                btn3.Visible = false;
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (decNumber > 0)
+            {
+                btn1.Visible = false;
+                btn2.Visible = true;
+                btn3.Visible = true;
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (decNumber > 0)
+            {
+                btn1.Visible = true;
+                btn2.Visible = false;
+                btn3.Visible = true;
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (decNumber > 0)
+            {
+                btn1.Visible = true;
+                btn2.Visible = true;
+                btn3.Visible = false;
+            }
+        }
+    }
+}
